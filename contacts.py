1cd8292339af368eaf92d967d5382d22dc5d0b6b
Erik Revaj <erik.revaj3@gmail.com>
2020-12-01 10:10:11 +0100

    added files

---
+++ contacts.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+db = sqlite3.connect("contacts.sqlite")
+db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("INSERT INTO contacts(name, phone, email) VALUES('Tim', 656556, 'tim@email.com')")
+db.execute("INSERT INTO contacts(name, phone, email) VALUES('Brian', 1234, 'brian@email.com')")
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM contacts")
+
+print(cursor.fetchone())
+for name, phone, email in cursor:
+    print(name)
+    print(phone)
+    print(email)
+    print("------------------------")
+
+db.close()
---
+++ contacts2.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+db = sqlite3.connect("contacts.sqlite")
+
+new_email = "newemail@update.com"
+phone = input("Please enter the phone number:")
+
+update_sql = "UPDATE contacts SET email = ? WHERE phone = ?"
+print(update_sql)
+
+update_cursor = db.cursor()
+update_cursor.execute(update_sql, (new_email, phone))
+print("{} rows updated".format(update_cursor.rowcount))
+
+print()
+print("are connections the same {}".format(update_cursor.connection == db))
+print()
+
+update_cursor.connection.commit()
+update_cursor.close()
+
+for name, phone, email in db.execute("SELECT * FROM contacts"):
+    print(name)
+    print(phone)
+    print(email)
+
+db.close()
